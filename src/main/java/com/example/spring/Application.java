d0d70519f118182abc5952337a0961181cb347e2
Гусева Екатерина Александровна <ek.guseva@bft.ru>
2024-10-03 10:54:52 +0300

    m2

--- src/main/java/com/example/spring/Application.java
+++ src/main/java/com/example/spring/Application.java
@@ -12,8 +12,8 @@ public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+		//ыыыы
 	}
- 		2222222
 	@Bean
 	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
 		return args -> {
